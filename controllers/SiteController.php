5387bbbfb3b3bb9c764cb856fc1a022d0163d306
lamaaa <haoyang.lin@foxmail.com>
2018-01-06 10:32:19 +0800

    fix: 删除SiteController中signup入口

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,18 +125,6 @@ class SiteController extends Controller
         return $this->render('about');
     }
 
-    public function actionSignup()
-    {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            return $this->goHome();
-        }
-
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
-
     public function actionManagement()
     {
         return $this->render('management', [
